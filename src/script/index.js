432049bb38ee442985a20d0079f9d15a2082d2c8
Tabiur Rahman <tabiurrahman7ev319@gmail.com>
2023-11-05 16:17:12 +0600

    Add files via upload

---
+++ src/script/index.js
@@ -0,0 +1,21 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const swipperItem = document.getElementsByClassName("swiper-wrapper");
+  swipperItem.style.width = "35%";
+});
+
+const swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
